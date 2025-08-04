32b3a5ee786a7c0240a062f6d3edaa603ffed212
dhruv-moradiya2 <dhruv.unikworkdevops@gmail.com>
2025-08-04 15:25:08 +0530

    deploy first cdk stack

--- s3_bucket/s3_bucket_stack.py
+++ s3_bucket/s3_bucket_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_s3 as bucket
+    aws_s3 as s3
 )
 from constructs import Construct
 
@@ -10,4 +10,7 @@ class S3BucketStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        
+
+        bucket = s3.Bucket(self, 
+                           "my-demo-bucekt", 
+                           bucket_name="demo-cdk--t-d-g")
