e0f853ca30a9c74ce99960adb798f928e4208e46
dhruv-moradiya2 <dhruv.unikworkdevops@gmail.com>
2025-08-04 14:36:16 +0530

    first commit

--- s3_bucket/s3_bucket_stack.py
+++ s3_bucket/s3_bucket_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as bucket
 )
 from constructs import Construct
 
@@ -10,10 +10,4 @@ class S3BucketStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "S3BucketQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
